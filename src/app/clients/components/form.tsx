eb5f4f4b09ab68193edac8341ac383b0b56828d9
Yuri Galvao <yuri.gvao@gmail.com>
2023-07-05 19:44:09 -0300

    created form Clients

---
+++ src/app/clients/components/form.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { FormComponent } from "@/common/Form"
+import { addClientInStore } from "@/store/clients"
+import { Client } from "@/store/types"
+
+import { ChangeEventHandler, FormEventHandler, useState } from "react"
+
+const initialValue: Client = {
+    name: ''
+}
+
+export const FormClient = () => {
+
+    const [client, setClient] = useState(initialValue)
+
+    const resetInputs = () => setClient(initialValue)
+
+    const onSubmit: FormEventHandler<HTMLFormElement> | undefined = (event) => {
+        event.preventDefault()
+        addClientInStore(client)
+        resetInputs()
+    }
+
+    const onChange: ChangeEventHandler<HTMLInputElement> | undefined = ({ target: { id, value } }) => 
+        setClient(state => ({ ...state, [id]: value }))
+
+    return (
+        <FormComponent.Root onSubmit={onSubmit}>
+            <FormComponent.Input name="name" id="name" state={client} onChange={onChange} />
+
+            <FormComponent.Button type="submit" >Enviar</FormComponent.Button>
+        </FormComponent.Root>
+    )
+}
+
