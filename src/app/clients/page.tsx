adda636f582991cd86ba2529967c2bcb55d7e26a
Yuri Galvao <yuri.gvao@gmail.com>
2023-07-05 19:44:34 -0300

    refactored

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,16 +1,5 @@
-import { ChevronRightIcon } from "@heroicons/react/24/solid";
-import styles from './styles.module.css'
-
-type Client = {
-    id: string;
-    fullName: string;
-}
-
-const clients: Client[] = [
-    { id: '1', fullName: 'Yuri Moreira Galvao Prado', },
-    { id: '2', fullName: 'Vitoria Erica Teixeira Lima', },
-    { id: '3', fullName: 'Alice Fernandes Galvão Prado', },
-]
+import { FormClient } from "./components/form";
+import { ListClients } from "./components/list";
 
 export default function ClientsPage() {
 
@@ -18,15 +7,8 @@ export default function ClientsPage() {
         <>
             <h1>Clients</h1>
 
-            <ul className={styles.card_wrapper} >
-                {clients.map(client => (
-                    <li key={client.id} className={`${styles.item} ${styles.card_item}`}>
-                        <h2>{client.fullName}</h2>
-                        <ChevronRightIcon className={styles.icon} />
-                    </li>
-                ))}
-
-            </ul>
+            <FormClient />
+            <ListClients />
         </>
     )
 }
