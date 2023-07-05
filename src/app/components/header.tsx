dee089507118ba7c9618f77507259732e3c9a563
Yuri Galvao <yuri.gvao@gmail.com>
2023-07-05 17:42:56 -0300

    created header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,17 @@
 import { Container } from "@/common/container";
 import styles from '../styles/layout.module.css'
+import { Bars2Icon } from "@heroicons/react/24/solid";
 
 export const Header = () => (
     <header className={styles.header} >
         <Container className={styles.headerContainer} >
-            Óptica Vitória
+            <a href="/">
+                Óptica Vitória
+            </a>
+
+            <span>
+                <Bars2Icon className={styles.icon} />
+            </span>
         </Container>
     </header>
 )
